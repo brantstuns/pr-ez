dafcf8f0c55da1844f75b8dab598798721fdcafa
Brant Stuns <brantstuns@gmail.com>
2016-12-09 00:57:33 -0700

    Initially commiting to making the pr's easier for all of us

---
+++ README.md
@@ -0,0 +1,1 @@
+A tool to automate opening a pull request on github 🐕
---
+++ pr-ez.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+print("wow pr's are so easy")
+brantsGithubUrl = 'http://github.com/brantstuns'
+webbrowser.open(brantsGithubUrl)
