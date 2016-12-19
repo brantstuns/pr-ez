7bd82ea002c728c26571ade8cb3736d5e46d9394
Brant Stuns <brantstuns@gmail.com>
2016-12-19 00:10:50 -0700

    Added flow to pull request page. Also added config parsing to first task to make sure users don't have to enter their github creds and then see an empty config error.

--- pr-ez.py
+++ pr-ez.py
@@ -6,11 +6,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
 
 def getConfigData(configFile):
     r = re.compile("\: (.*)")
     config = [r.search(config.rstrip('\n')).group(1) for config in open(configFile)]
-    print config
+    print 'yo2'
     if None in config:
         print '\n\nYou need to completely fill out config.txt for this to be ez :(\n\n'
         sys.exit()
@@ -23,38 +25,46 @@ def loginToGithub (u, p, driver):
     driver.find_element_by_id('password').send_keys(p)
     driver.find_element_by_class_name('btn-primary').click()
 
-def openNewPr(driver, githubRepo, assignees):
+def openNewPr(driver, githubRepo, assignees, branch):
     driver.get(githubRepo)
     driver.find_element_by_class_name('new-pull-request-btn').click()
     compareBranch = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'compare'))
+        EC.presence_of_element_located((By.XPATH, "//div[button//i[contains(text(), 'compare')]]"))
     )
-    driver.find('compare').click()
-    print 'yo2'
+    driver.find_element_by_xpath("//div[button//i[contains(text(), 'compare')]]").click()
+    driver.find_elements_by_xpath("//*[contains(@id, 'commitish-filter-field')]")[1].send_keys(branch)
+    driver.find_elements_by_xpath("//div[contains(text(), {0})]".format(branch))[1].click()
+    for dev in assignees:
+        driver.find_elements_by_class_name('.sidebar-assignee')[1].click()
+        driver.find_element_by_id('assignee-filter-field').send_keys(dev)
+        driver.find_element_by_class_name('.navigation-focus').send_keys(Keys.RETURN)
 
-def browserStuff(driver, gitCredentials):
-    configData = getConfigData('config.txt')
+def browserStuff(driver, gitCredentials, branch, configData):
     githubRepo = configData[0]
     assignees = configData[1].split(', ')
     try:
         loginToGithub(gitCredentials[0], gitCredentials[1], driver)
-        openNewPr(driver, githubRepo, assignees)
+        openNewPr(driver, githubRepo, assignees, branch)
     except Exception as e:
         print 'Exception: ' + e.message
 
 
 def main():
-    if raw_input('\nWant to make this PR really easy? ').lower().startswith('n'):
-        sys.exit()
+    config = getConfigData('config.txt')
+    if len(sys.argv) == 1:
+        branch = raw_input('\nEnter the name of the remote branch you want to open a Pull Request for (case matters): ')
+    else:
+        branch = sys.argv[1]
+
     gitEmail = raw_input('\nEnter your github email: ').lower().strip()
-    gitPass = getpass.getpass(prompt = 'password (dont worry, this isnt stored anywhere and wont get echoed to the console): ')
+    gitPass = getpass.getpass(prompt = '\npassword (dont worry, this isnt stored anywhere and wont get echoed to the console): ')
     chromedriver = '/usr/local/bin/chromedriver'
     os.environ['webdriver.chrome.driver'] = chromedriver
     driver = webdriver.Chrome(chromedriver)
     try:
-        browserStuff(driver, [gitEmail, gitPass])
+        browserStuff(driver, [gitEmail, gitPass], branch, config)
     except Exception as err:
-        print 'Exception: ' + err.message
+        print '\nException: ' + err.message
         driver.close()
 
 main()
